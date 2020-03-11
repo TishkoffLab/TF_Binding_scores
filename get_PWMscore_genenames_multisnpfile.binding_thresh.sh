7cba6d0d66a7f349f8ae1de41191f43ee728038c
Jake Haut <jakehaut@gmail.com>
2020-03-11 14:42:52 -0400

    updated the script to use the 1000 replicate Z score file

--- get_PWMscore_genenames_multisnpfile.binding_thresh.sh
+++ get_PWMscore_genenames_multisnpfile.binding_thresh.sh
@@ -38,6 +38,6 @@ while read line; do
   curr_ref=(`echo "${line}" | cut -d$'\t' -f 4`)
   curr_alt=(`echo "${line}" | cut -d$'\t' -f 5`)
   curr_outname="${outname}.chrm${curr_chrm}.pos${curr_pos}"
-  python ${script_path}/get_PWMscores.py -o "${curr_outname}.above_threshold.PWM_scores" -m "${script_path}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_transfac" -p "${curr_chrm}:${curr_pos}" -r "${curr_ref}" -a "${curr_alt}" -c "${genomes_loc}/chr${curr_chrm}.fa" -b "${script_path}/ACTG_count.all_chrms.fractions.txt" -z "${script_path}/backgroundZ_forTFs.100reps.txt" -f "0.01" #"${argflags_string}"
+  python ${script_path}/get_PWMscores.py -o "${curr_outname}.above_threshold.PWM_scores" -m "${script_path}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_transfac" -p "${curr_chrm}:${curr_pos}" -r "${curr_ref}" -a "${curr_alt}" -c "${genomes_loc}/chr${curr_chrm}.fa" -b "${script_path}/ACTG_count.all_chrms.fractions.txt" -z "${script_path}/backgroundZ_forTFs.1000reps.txt" -f "0.01" #"${argflags_string}"
 done <"${snp_file}"
 
